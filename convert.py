a95b4165ece72b6de79755b426cf0b6749f01c41
Albert Wang <git@albertyw.com>
2018-02-19 21:27:13 -0800

    Make convert.py accept passable configs

--- convert.py
+++ convert.py
@@ -6,38 +6,51 @@ There are a bunch of configurable variables
 import csv
 from icalendar import Calendar, Event
 
+DEFAULT_CONFIG = {
+    'HEADER_COLUMNS_TO_SKIP':  0,
+
+    # The variables below refer to the column indexes in the CSV
+    'CSV_NAME': 0,
+    'CSV_START_DATE': 1,
+    'CSV_END_DATE': 1,
+    'CSV_DESCRIPTION': 2,
+    'CSV_LOCATION': 3,
+}
 
 class ConvertCSVToICal():
     def __init__(self):
-        self.HEADER_COLUMNS_TO_SKIP = 0
-
-        # The variables below refer to the column indexes in the CSV
-        self.NAME = 0
-        self.START_DATE = 1
-        self.END_DATE = 1
-        self.DESCRIPTION = 2
-        self.LOCATION = 3
-
         self.csv_data = []
-        self.cal = Calendar()
+        self.cal = None
 
-    def read_csv(self, csv_location):
+    def _generate_configs_from_default(self, overrides=None):
+        """ Generate configs by inheriting from defaults """
+        config = DEFAULT_CONFIG
+        if not overrides:
+            overrides = {}
+        for k, v in overrides.items():
+            config[k] = v
+        return config
+
+    def read_csv(self, csv_location, csv_configs=None):
         """ Read the csv file """
+        csv_configs = self._generate_configs_from_default(csv_configs)
         with open(csv_location, 'rb') as csv_file:
             csv_reader = csv.reader(csv_file)
             self.csv_data = list(csv_reader)
-        self.csv_data = self.csv_data[self.HEADER_COLUMNS_TO_SKIP:]
+        self.csv_data = self.csv_data[csv_configs['HEADER_COLUMNS_TO_SKIP']:]
         return self.csv_data
 
-    def make_ical(self):
+    def make_ical(self, csv_configs=None):
         """ Make iCal entries """
+        csv_configs = self._generate_configs_from_default(csv_configs)
+        self.cal = Calendar()
         for row in self.csv_data:
             event = Event()
-            event.add('summary', row[self.NAME])
-            event.add('dtstart', row[self.START_DATE])
-            event.add('dtend', row[self.END_DATE])
-            event.add('description', row[self.DESCRIPTION])
-            event.add('location', row[self.LOCATION])
+            event.add('summary', row[csv_configs['CSV_NAME']])
+            event.add('dtstart', row[csv_configs['CSV_START_DATE']])
+            event.add('dtend', row[csv_configs['CSV_END_DATE']])
+            event.add('description', row[csv_configs['CSV_DESCRIPTION']])
+            event.add('location', row[csv_configs['CSV_LOCATION']])
             self.cal.add_component(event)
         return self.cal
 
