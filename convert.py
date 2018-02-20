231ec7e583f181d609ef0f82dda8fa9cc2211316
Albert Wang <git@albertyw.com>
2018-02-19 17:50:17 -0800

    Clean up python

--- convert.py
+++ convert.py
@@ -1,31 +1,30 @@
 """
-This file reads the CSV file and saves an ical file.  
+This file reads the CSV file and saves an ical file.
 There are a bunch of configurable variables
 """
 
 import csv
 from icalendar import Calendar, Event
 
+
 class Convert():
-    # Initialize some configs
     def __init__(self):
         self.CSV_FILE_LOCATION = None
         self.SAVE_LOCATION = None
         self.HEADER_COLUMNS_TO_SKIP = 0
-        
+
         # The variables below refer to the column indexes in the CSV
         self.NAME = 0
         self.START_DATE = 1
         self.END_DATE = 1
         self.DESCRIPTION = 2
         self.LOCATION = 3
-        
+
         self.csv_data = []
         self.cal = Calendar()
-        
-    # Read the csv file
-    #
+
     def read_csv(self):
+        """ Read the csv file """
         csv_reader = csv.reader(open(self.CSV_FILE_LOCATION, 'rb'))
         i = 0
         for row in csv_reader:
@@ -34,10 +33,9 @@ class Convert():
                 continue
             self.csv_data.append(row)
         return self.csv_data
-        
-    # Make iCal entries
-    #
+
     def make_ical(self):
+        """ Make iCal entries """
         for row in self.csv_data:
             event = Event()
             event.add('summary', row[self.NAME])
@@ -47,11 +45,9 @@ class Convert():
             event.add('location', row[self.LOCATION])
             self.cal.add_component(event)
         return self.cal
-        
-    # Save the calendar instance to a file
-    #
+
     def save_ical(self):
+        """ Save the calendar instance to a file """
         f = open(self.SAVE_LOCATION, 'wb')
         f.write(self.cal.to_ical())
         f.close()
-        
