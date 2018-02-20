d576992c13ccf942556ebab99d23603ca5215d92
Albert Wang <git@albertyw.com>
2018-02-19 18:59:24 -0800

    Fix Flake8 errors

--- examples/day.py
+++ examples/day.py
@@ -2,13 +2,13 @@
 This file is an exmaple for running the conversion script
 """
 
+from datetime import datetime
 import sys
-# Yeah this is a hack
+
 sys.path.append('.')
 sys.path.append('../')
 
-from convert import ConvertCSVToICal
-from datetime import datetime
+from convert import ConvertCSVToICal  # NOQA
 
 convert = ConvertCSVToICal()
 convert.CSV_FILE_LOCATION = 'examples/BostonCruiseTerminalSchedule.csv'
@@ -22,7 +22,10 @@ convert.LOCATION = 9
 
 convert.read_csv()
 for row in convert.csv_data:
-    row[convert.START_DATE] = datetime.strptime(row[convert.START_DATE], '%m/%d/%y').date()
-    
+    row[convert.START_DATE] = datetime.strptime(
+        row[convert.START_DATE],
+        '%m/%d/%y'
+    ).date()
+
 convert.make_ical()
 convert.save_ical()
