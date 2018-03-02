9fd0ff0d52e67f0e54a305e635bc6d663a2d3ec1
Albert Wang <git@albertyw.com>
2018-03-02 01:40:26 -0800

    Add test case

---
+++ csv_ical/tests/test_convert.py
@@ -0,0 +1,10 @@
+import unittest
+
+from csv_ical import convert
+
+class TestConvert(unittest.TestCase):
+    def setUp(self):
+        self.convert = convert.Convert()
+
+    def test_generate_configs(self):
+        self.convert._generate_configs_from_default()
