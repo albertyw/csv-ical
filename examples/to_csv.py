5101164cb4afbb013130fe8e867c530996df956f
Philipp <philipp@smponias.de>
2020-11-26 18:16:46 +0100

    Better user experience - files are given with the script call

--- examples/to_csv.py
+++ examples/to_csv.py
@@ -3,11 +3,12 @@ Example conversion of ical to csv
 """
 
 from csv_ical import Convert
+import sys
 
 
 convert = Convert()
-convert.CSV_FILE_LOCATION = 'examples/BostonCruiseTerminalSchedule.csv'
-convert.SAVE_LOCATION = 'examples/arrive.ics'
+convert.CSV_FILE_LOCATION = sys.argv[2]
+convert.SAVE_LOCATION = sys.argv[1]
 
 convert.read_ical(convert.SAVE_LOCATION)
 convert.make_csv()
