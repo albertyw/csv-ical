26b5e38dc6ae4c8082037a4757bb8dc130ee8be7
Albert Wang <git@albertyw.com>
2020-12-26 00:51:28 -0800

    Explicitly support python 3.8 and 3.9 and python typing

--- setup.py
+++ setup.py
@@ -43,10 +43,13 @@ setup(
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
     ],
 
     keywords='',
 
+    package_data={"csv_ical": ["py.typed"]},
     packages=find_packages(exclude=["tests"]),
 
     python_requires='>=3.5',
