3f590d0afb3ffefaf43b60775a5bc086beb10e92
Albert Wang <git@albertyw.com>
2022-03-14 22:49:21 -0400

    Drop official support for python 3.5 and 3.6

--- setup.py
+++ setup.py
@@ -41,8 +41,6 @@ setup(
         'License :: OSI Approved :: MIT License',
 
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
